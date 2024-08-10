c58245111d1d65f465dfe2e9907041917418c55c
Antonio Saborido <antonio.saborido01@gmail.com>
2024-08-10 15:58:00 +0200

    ssr funcionando

--- src/app/services/translations.service.ts
+++ src/app/services/translations.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { PlatformService } from './platform.service';
 
 @Injectable({
   providedIn: 'root'
@@ -7,15 +8,22 @@ import { TranslateService } from '@ngx-translate/core';
 export class TranslationService {
   private readonly LANGUAGE_KEY = 'app_language';
 
-  constructor(private translate: TranslateService) {
-    const savedLanguage = localStorage.getItem(this.LANGUAGE_KEY) || this.translate.getBrowserLang() || 'es';
+  constructor(private translate: TranslateService, private platformService: PlatformService) {
+    let savedLanguage = 'es';
+    if (this.platformService.isBrowser()) {
+      savedLanguage = localStorage.getItem(this.LANGUAGE_KEY) || this.translate.getBrowserLang() || 'es';
+    } else {
+      savedLanguage = this.translate.getBrowserLang() || 'es';
+    }
     this.translate.setDefaultLang('es');
     this.translate.use(savedLanguage);
   }
 
   changeLanguage(lang: string) {
     this.translate.use(lang);
-    localStorage.setItem(this.LANGUAGE_KEY, lang);
+    if (this.platformService.isBrowser()) {
+      localStorage.setItem(this.LANGUAGE_KEY, lang);
+    }
   }
 
   get currentLang() {
